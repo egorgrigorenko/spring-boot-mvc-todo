a2918729682f6116f7221231b387eb91d12dd4ac
Egor Grigorenko <iopochta@gmail.com>
2018-07-30 17:49:10 +0300

    spring boot initializer web kotlin gradle

---
+++ src/main/kotlin/com/eg/springbootmvctodo/SpringBootMvcTodoApplication.kt
@@ -0,0 +1,11 @@
+package com.eg.springbootmvctodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootMvcTodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootMvcTodoApplication>(*args)
+}
---
+++ src/test/kotlin/com/eg/springbootmvctodo/SpringBootMvcTodoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.eg.springbootmvctodo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootMvcTodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
